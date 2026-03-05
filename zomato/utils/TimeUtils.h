a154f09fd6575e7531a687376bac72657f3ba01c
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2026-03-05 12:43:06 +0530

    Add TimeUtils class for retrieving the current time

---
+++ zomato/utils/TimeUtils.h
@@ -0,0 +1,17 @@
+#pragma once 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class TimeUtils{
+    public:
+        static string getCurrentTime()
+        {
+            time_t now = time(0);
+            char* dt = ctime(&now);
+            string s(dt);
+            if (!s.empty() && s.back() == '\n')
+            s.pop_back();
+            return s;
+        }
+};
