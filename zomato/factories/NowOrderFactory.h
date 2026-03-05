7bbd05ba8e066f80c826c1afaf7fe6bdd150adf3
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2026-03-05 16:25:12 +0530

    Add NowOrderFactory class for creating delivery and pickup orders

---
+++ zomato/factories/NowOrderFactory.h
@@ -0,0 +1,36 @@
+#pragma once 
+
+#include<bits/stdc++.h>
+#include<Order.h>
+#include<TimeUtils.h>
+#include<PickupOrder.h>
+#include<DeliveryOrder.h>
+using namespace std;
+
+class NowOrderFactory : public Order 
+{
+    public:
+        Order* createOrder(User* user, Cart* cart, Restaurant* restaurant, const vector<MenuItem>&menuItems, 
+                            PaymentStrategy* paymentStrategy, double totalCost, const string& orderType){
+                Order* order = nullptr;
+                if(orderType == "Delivery")
+                {
+                    auto deliveryOrder = new DeliveryOrder();
+                    deliveryOrder->setUserAddress(user->getAddress());
+                    order = deliveryOrder;
+                }
+                else
+                {
+                    auto pickupOrder = new PickupOrder();
+                    pickupOrder->setRestaurantAddress(restaurant->getLocation());
+                    order = pickupOrder;
+                }
+                order->setUser(user);
+                order->setRestaurant(restaurant);
+                order->setItems(menuItems);
+                order->setPaymentStrategy(paymentStrategy);
+                order->setScheduled(TimeUtils::getCurrentTime());
+                order->setTotal(totalCost);
+                return order;
+        }
+};
