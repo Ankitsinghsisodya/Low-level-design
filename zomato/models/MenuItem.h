e5234f1e01e88eb25e69ac23618ee0774065a9ee
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2026-03-05 12:42:41 +0530

    Refactor MenuItem class for improved readability and consistency

--- zomato/models/MenuItem.h
+++ zomato/models/MenuItem.h
@@ -1,41 +1,44 @@
 #pragma once
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class MenuItem{
-    private:
-        int menuId, price;
-        string itemName;
-    public:
-        MenuItem(int menuId, int price, string itemName){
-            this->menuId = menuId;
-            this->price = price;
-            this->itemName = itemName;
-        }
+class MenuItem
+{
+private:
+    int menuId, price;
+    string itemName;
 
-        void setMenuId(int menuId)
-        {
-            this->menuId = menuId;
-        }
-        int getMenuId()
-        {
-            return menuId;
-        }
-        void setPrice(int price)
-        {
-            this->price = price;
-        }
-        int getPrice()
-        {
-            return price;
-        }
-        void setIemName(string itemName)
-        {
-            this->itemName = itemName;
-        }
-        string getItemName(){
-            return itemName;
-        }
+public:
+    MenuItem(int menuId, int price, string itemName)
+    {
+        this->menuId = menuId;
+        this->price = price;
+        this->itemName = itemName;
+    }
 
+    void setMenuId(int menuId)
+    {
+        this->menuId = menuId;
+    }
+    int getMenuId() const
+    {
+        return menuId;
+    }
+    void setPrice(int price)
+    {
+        this->price = price;
+    }
+    double getPrice() const
+    {
+        return price;
+    }
+    void setIemName(string itemName)
+    {
+        this->itemName = itemName;
+    }
+    string getItemName() const
+    {
+        return itemName;
+    }
 };
