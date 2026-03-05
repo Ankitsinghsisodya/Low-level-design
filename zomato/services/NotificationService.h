fba00b50d55fe5dac68e7861a43075ba85255f23
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2026-03-05 16:40:42 +0530

    Add NotificationService class for handling order notifications

---
+++ zomato/services/NotificationService.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <bits/stdc++.h>
+using namespace std;
+#include<Order.h>
+#include<MenuItem.h>
+
+class NotificatoinService
+{
+public:
+    static void notify(Order *order)
+    {
+        cout << "\nNotification: New " << order->getType() << " order placed!" << endl;
+        cout << "---------------------------------------------" << endl;
+        cout << "Order ID: " << order->getOrderId() << endl;
+        cout << "Customer: " << order->getUser()->getName() << endl;
+        cout << "Restaurant: " << order->getRestaurant()->getName() << endl;
+        cout << "Items Ordered:\n";
+
+        const vector<MenuItem> &items = order->getItems();
+        for (const auto &item : items)
+        {
+            cout << "   - " << item.getItemName() << " (₹" << item.getPrice() << ")\n";
+        }
+
+        cout << "Total: ₹" << order->getTotal() << endl;
+        cout << "Scheduled For: " << order->getScheduled() << endl;
+        cout << "Payment: Done" << endl;
+        cout << "---------------------------------------------" << endl;
+    }
+};
