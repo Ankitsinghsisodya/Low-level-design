1841de4c642fa1fa152ee788cee36eec1057fe4f
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2026-03-05 15:07:19 +0530

    Add PickupOrder class for managing pickup orders and restaurant address

---
+++ zomato/models/PickupOrder.h
@@ -0,0 +1,27 @@
+#pragma once 
+
+#include<bits/stdc++.h>
+#include<Order.h>
+using namespace std;
+
+class PickupOrder: public Order {
+    private:
+        string restaurantAddress;
+    public:
+        PickupOrder()
+        {
+            restaurantAddress = "";
+        }
+        string getType() const override 
+        {
+            return "Pickup";
+        }
+        void setRestaurantAddress(const string& addr)
+        {
+            restaurantAddress = addr;
+        }
+        string getRestaurantAddress() const 
+        {
+            return restaurantAddress;
+        }
+};
