323937372f0189283cf83dce4df17dcd979e4a23
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2026-03-05 14:28:29 +0530

    Implement Order class for managing orders, including payment processing and item management

---
+++ zomato/models/Order.h
@@ -0,0 +1,115 @@
+#pragma once 
+
+#include<bits/stdc++.h>
+#include<User.h>
+#include<Restaurant.h>
+#include<MenuItem.h>
+#include<PaymentStrategy.h>
+using namespace std; 
+
+class Order{
+    protected:
+        static int nextOrderId;
+        int orderId;
+        User* user;
+        Restaurant* restaurant;
+        vector<MenuItem>items;
+        PaymentStrategy* paymentStrategy;
+        double total;
+        string scheduled;
+    public:
+        Order()
+        {
+            user = nullptr;
+            restaurant = nullptr;
+            paymentStrategy = nullptr;
+            total = 0.0;
+            scheduled = "";
+            orderId = ++nextOrderId;
+        }
+
+        virtual ~Order()
+        {
+            delete paymentStrategy;
+        }
+
+        bool processPayment()
+        {
+            if(paymentStrategy)
+            {
+                paymentStrategy->pay(total);
+                return true;
+            }
+            else 
+            {
+                cout << "Choose a payment strategy first\n";
+                return false;
+            }
+        }
+
+        virtual string getType() const = 0;
+
+        int getOrderId () const 
+        {
+            return orderId;
+        }
+
+        void setUser(User* user) 
+        {
+            this->user = user;
+        }
+
+        User* getUser() const 
+        {
+            return user;
+        }
+
+        void setRestaurant(Restaurant* r)
+        {
+            restaurant = r;
+        }
+
+        Restaurant* getRestaurant() const
+        {
+            return restaurant;
+        }
+
+        void setItems(const vector<MenuItem>&its)
+        {
+            items = its;
+            total = 0;
+            for(auto &item : items)
+                total += item.getPrice();
+        }
+
+        const vector<MenuItem>& getItems () const
+        {
+            return items;
+        }
+
+        void setPaymentStrategy(PaymentStrategy* paymentStrategy)
+        {
+            this->paymentStrategy = paymentStrategy;
+        }
+        void setScheduled(const string& s)
+        {
+            scheduled = s;
+        }
+
+        string getScheduled() const 
+        {
+            return scheduled;
+        }
+
+        double getTotal() const 
+        {
+            return total;
+        }
+
+        void setTotal(double total)
+        {
+            this->total = total;
+        }
+};
+
+int Order::nextOrderId = 0;
