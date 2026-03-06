2dba979146b230944b8340f46aca3cb32d26523b
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2026-03-06 20:19:56 +0530

    feat: add main entry point for NotificationSystem

---
+++ NotificationSystem/main.cpp
@@ -0,0 +1,35 @@
+#include "models/SimpleNotification.h"
+#include "decorators/TimeStampDecorator.h"
+#include "decorators/SignatureDecorator.h"
+#include "observers/Logger.h"
+#include "observers/NotificationEngine.h"
+#include "strategies/EmailStrategy.h"
+#include "strategies/SMSStrategy.h"
+#include "strategies/PopUpStrategy.h"
+#include "services/NotificationService.h"
+
+int main()
+{
+    NotificationService *notificationService = NotificationService::getInstance();
+    NotificationObservable *notificationObservable = notificationService->getObservable();
+
+    Logger *logger = new Logger(notificationObservable);
+    NotificationEngine *notificationEngine = new NotificationEngine(notificationObservable);
+
+    notificationEngine->addNotificationStrategy(new EmailStrategy("ankitsingh24012005@gmail.com"));
+    notificationEngine->addNotificationStrategy(new SMSStrategy("7667144864"));
+    notificationEngine->addNotificationStrategy(new PopUpStrategy());
+
+    notificationObservable->addObserver(logger);
+    notificationObservable->addObserver(notificationEngine);
+
+    INotification *notification = new SimpleNotification("Your order has been shipped!");
+    notification = new TimeStampDecorator(notification);
+    notification = new SignatureDecorator(notification, "ankit sisodya");
+
+    notificationService->sendNotification(notification);
+
+    delete logger;
+    delete notificationEngine;
+    return 0;
+}
