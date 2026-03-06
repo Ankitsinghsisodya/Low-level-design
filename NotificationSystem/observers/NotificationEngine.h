3e87cb5256d187dfc0221ab7ade20429dd65fa6a
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2026-03-06 20:19:06 +0530

    feat: add NotificationEngine observer

---
+++ NotificationSystem/observers/NotificationEngine.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "IObserver.h"
+#include "NotificationObservable.h"
+#include "../strategies/INotificationStrategy.h"
+using namespace std;
+
+class NotificationEngine : public IObserver
+{
+private:
+    NotificationObservable *notificationObservable;
+    vector<INotificationStrategy *> notificationStrategies;
+
+public:
+    NotificationEngine(NotificationObservable *observable) : notificationObservable(observable) {}
+
+    void addNotificationStrategy(INotificationStrategy *ns)
+    {
+        notificationStrategies.push_back(ns);
+    }
+
+    void update() override
+    {
+        string content = notificationObservable->getNotificationContent();
+        for (auto strategy : notificationStrategies)
+            strategy->sendNotification(content);
+    }
+};
