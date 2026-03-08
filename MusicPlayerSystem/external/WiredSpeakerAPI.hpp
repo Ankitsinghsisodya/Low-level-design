1047bcb68c8ebb0b29e7fb5514a9ba057f50380d
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2026-03-08 15:26:13 +0530

    feat: add WiredSpeakerAPI class for audio playback via wired connection

---
+++ MusicPlayerSystem/external/WiredSpeakerAPI.hpp
@@ -0,0 +1,13 @@
+#pragma once 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class WiredSpeakerAPI
+{
+    public:
+        void playSoundViaCable(const string& data)
+        {
+            cout << "[WiredSpeaker] Playing: " << data << "\n";
+        }
+};
